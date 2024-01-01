db7c1d23d971f67854c535942ced8c97ccd0a219
Stephen Wang <stephenwzz77@gmail.com>
2024-01-01 17:16:46 +0800

    frontend: optimize categories

--- shareme_frontend/src/components/Feed.jsx
+++ shareme_frontend/src/components/Feed.jsx
@@ -29,7 +29,13 @@ const Feed = () => {
     }
   }, [categoryId]);
 
-  if (loading) return <Spinner message="We are adding new ideas to your feed!" />
+  const ideaName = categoryId || 'new';
+  if (loading) {
+    return (
+      <Spinner message={`We are adding ${ideaName} ideas to your feed!`} />
+    );
+  }
+  if (!pins?.length) return <h2>No pins available</h2>
   return (
     <div>
       {pins && (
